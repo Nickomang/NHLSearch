a9dbff8f3aa24350dea52d3f2544f018f55162fd
Nicolas Oman <nickomang@gmail.com>
2016-04-05 11:37:32 -0700

    now works for 20132014 through 20142015 seasons

--- test.py
+++ test.py
@@ -16,6 +16,12 @@
 
 #so lets test the ducks.
 
+
+# Earliest we can do is 20132014
+# Latest we can do is 20142015
+
+
+
 import json
 import json
 from bson import json_util, ObjectId
@@ -23,14 +29,12 @@ import sys
 import requests
 import re
 
-
-
-print "testing for the Calgary in March (03) of 2015"
-
-team = "CGY"
+team = "ANA"
 year = "2015"
 fullyear = "20142015"
-month = "03"
+month = "02"
+
+print "Testing for the team " + team + " in the year " + fullyear + ", " + month
 
 def get_game_ids(team, year, month):
 	url = "http://nhlwc.cdnak.neulion.com/fs1/nhl/league/clubschedule/" + team + "/" + year + "/" + month + "/iphone/clubschedule.json"
@@ -42,23 +46,23 @@ def get_game_ids(team, year, month):
 	game_count = 0
 	for game in response['games']:
 		game_ids.append(str(game['gameId']))
-
+	print game_ids
 	return game_ids
 
-def get_ext_ids(game_id, year):
-	url = "http://live.nhle.com/GameData/" + year + "/" + game_id + "/gc/gcgm.jsonp"
+def get_ext_ids(game_id, fullyear):
+	url = "http://live.nhle.com/GameData/" + fullyear + "/" + game_id + "/gc/gcgm.jsonp"
 	response = requests.get(url).text
-	trimmed_response = response[10:-1]
-
+	if (fullyear == "20142015"):
+		trimmed_response = response[10:-1]
+	else:
+		trimmed_response = response[10:-2]
+	print 
 	json_response = json.loads(trimmed_response)
-
 	ext_ids = []
 
 	for event in json_response['video']['events']:
 		for feed in event['feeds']:
 			ext_ids.append(str(feed['extId']))
-
-
 
 	return ext_ids
 
