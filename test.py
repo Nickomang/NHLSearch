641043d7e0c7b7652b2b74c607c3c17fe7c2604b
Nicolas Oman <nickomang@gmail.com>
2016-04-05 12:06:50 -0700

    can now parse for goals, saves, and events with a given player

--- test.py
+++ test.py
@@ -34,6 +34,8 @@ year = "2016"
 fullyear = "20152016"
 month = "01"
 
+playername = "Corey Perry"
+
 print "Testing for the team " + team + " in the year " + fullyear + ", " + month
 
 def get_game_ids(team, year, month):
@@ -67,6 +69,49 @@ def get_ext_ids(game_id, fullyear):
 	print ext_ids
 	return ext_ids
 
+# Returns the ext_ids of events involving the player named
+def parse_for_player(playername, ext_ids):
+	highlight_urls = []
+	p = re.compile('.*(' + playername + ').*')
+	for ext_id in ext_ids:
+		highlight_desc = get_description_of_event(ext_id)
+		if p.match(highlight_desc):
+			highlight_url = get_highlight_url(ext_id)
+			highlight_urls.append(highlight_url)
+	return highlight_urls
+
+
+
+
+# Returns the URLs to Goals from the passed array of events
+def parse_for_goals(ext_ids):
+	p = re.compile('.*(goal).*')
+	highlight_urls = []
+	for ext_id in ext_ids:
+		highlight_url = get_highlight_url(ext_id)
+		if p.match(highlight_url):
+			highlight_urls.append(highlight_url)
+	return highlight_urls
+
+def parse_for_saves(ext_ids):
+	p = re.compile('.*(save).*')
+	highlight_urls = []
+	for ext_id in ext_ids:
+		highlight_url = get_highlight_url(ext_id)
+		if p.match(highlight_url):
+			highlight_urls.append(highlight_url)
+	return highlight_urls
+
+def parse_for_both(playername, event_type, ext_ids):
+	p1 = re.compile('.*(' + event_type + ').*')
+	p2 = re.compile('.*(' + playername + ').*')
+
+
+def get_description_of_event(ext_id):
+	url = "http://video.nhl.com/videocenter/servlets/playlist?ids=" + ext_id + "&format=json"
+	response = requests.get(url).json()
+	return response[0]['name']
+
 def get_highlight_url(ext_id):
 	url = "http://video.nhl.com/videocenter/servlets/playlist?ids=" + ext_id + "&format=json"
 	response = requests.get(url).json()
@@ -78,19 +123,15 @@ def get_highlight_url(ext_id):
 game_ids  = get_game_ids(team, year, month)
 print 
 ext_ids = get_ext_ids(game_ids[2], fullyear)
-
-highlight_urls = []
-
-# Could change to only get english and home links
-p = re.compile('.*(goal).*')
-
-for ext_id in ext_ids:
-	highlight_url = get_highlight_url(ext_id)
-	if p.match(highlight_url):
-		highlight_urls.append(highlight_url)
+set1 = parse_for_player("Corey Perry", ext_ids)
+set2 = parse_for_goals(ext_ids)
 
 
-print highlight_urls
+
+# print get_description_of_event(ext_ids[2])
+# print parse_for_goals(ext_ids)
+
+
 
 
 
