2896f754bf9a4a1879b9c2cf1dcf38a202b433b8
Nicolas Oman <nickomang@gmail.com>
2016-04-05 19:32:33 -0700

    switched year and month to be ints instead of strings

--- test.py
+++ test.py
@@ -8,47 +8,66 @@ event_dict = {'hit': 503, 'goal': 505, 'save': 506}
 start_time = time.time()
 
 team = "ANA"
-year = "2016"
-fullyear = "20152016"
+year = 2014
+fullyear = str(year-1)+str(year)
+print fullyear
 month = "01"
+month = 1
 
 playername = "Corey Perry"
 
 event_type = 'hit'
 location = 'h'
 
-print "Testing for the team " + team + " in the year " + fullyear + ", " + month
+print "Testing for the team " + team + " in the year " + fullyear + ", " + str(month).zfill(2)
 print "Looking for " + playername + " " + event_type + "s."  
 
 
 # Returns a list of the game ids for a given team during a given year and month
 def get_game_ids(team, year, month):
-	url = "http://nhlwc.cdnak.neulion.com/fs1/nhl/league/clubschedule/" + team + "/" + year + "/" + month + "/iphone/clubschedule.json"
-
+	if (month > 7):
+		url = "http://nhlwc.cdnak.neulion.com/fs1/nhl/league/clubschedule/" + team + "/" + str(year - 1) + "/" + str(month).zfill(2) + "/iphone/clubschedule.json"
+	else:
+		url = "http://nhlwc.cdnak.neulion.com/fs1/nhl/league/clubschedule/" + team + "/" + str(year) + "/" + str(month).zfill(2) + "/iphone/clubschedule.json"
 	game_ids = []
-
 	response = requests.get(url).json()
-
-	game_count = 0
 	for game in response['games']:
 		game_ids.append(str(game['gameId']))
 	return game_ids
 
+def get_game_ids_full(team, year):
+	game_ids = []
+	if year == 2016:
+		# Need to get october goals
+		for month in (11,12,1,2):
+			if (month > 7):
+				url = "http://nhlwc.cdnak.neulion.com/fs1/nhl/league/clubschedule/" + team + "/" + str(year-1) + "/" + str(month).zfill(2) + "/iphone/clubschedule.json"
+			else:
+				url = "http://nhlwc.cdnak.neulion.com/fs1/nhl/league/clubschedule/" + team + "/" + str(year) + "/" + str(month).zfill(2) + "/iphone/clubschedule.json"
+			response = requests.get(url).json()
+			for game in response['games']:
+				game_ids.append(str(game['gameId']))
+	else:
+		for month in (11,12,1,2,3,4,5):
+			url = "http://nhlwc.cdnak.neulion.com/fs1/nhl/league/clubschedule/" + team + "/" + str(year-1) + "/" + str(month).zfill(2) + "/iphone/clubschedule.json"
+			response = requests.get(url).json()
+			for game in response['games']:
+				game_ids.append(str(game['gameId']))
+	return game_ids
 # 
 def get_ext_ids(game_id, fullyear, event_num, location):
 	url = "http://live.nhle.com/GameData/" + fullyear + "/" + game_id + "/gc/gcgm.jsonp"
 	response = requests.get(url).text
-	if (fullyear == "20142015" or fullyear == "20152016"):
+	if (str(fullyear) == "20142015" or fullyear == "20152016"):
 		trimmed_response = response[10:-1]
 	else:
 		trimmed_response = response[10:-2]
+
 	json_response = json.loads(trimmed_response)
 	ext_ids = []
-
 	# 503 hits?
 	# If goals, 505
 	# If saves, 506
-
 	for event in json_response['video']['events']:
 		# if (event['type'] == event_type):
 		if (event['type'] == event_num):
@@ -134,6 +153,9 @@ for game_id in game_ids:
 print len(ext_ids)
 print parse_for_both(playername, event_type, ext_ids)
 print "Took ", time.time() - start_time, " to run."
+
+
+
 
 # Average times :
 	# hits   -> 15s
