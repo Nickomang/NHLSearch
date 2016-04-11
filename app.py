725b0963cbdbe955e9756b681ae87b578f429ddf
Nicolas Oman <nickomang@gmail.com>
2016-04-11 10:19:50 -0700

    variable passing for template now works

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-from flask import Flask, render_template
-import engine
-import example1
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return render_template('index.html')
+@app.route("/", methods=['GET', 'POST'])
+def index():
+	if request.method == "POST":
+		name = request.form['player']
+		return render_template("index.html", playername = name)
+	return render_template("index.html")
 
-@app.route('/search')
-def search():
-	return flask.jsonify(**example1)
-    # if request.method == "POST":
-    #     return example2
-    # return render_template('search.html')
+if __name__ == '__main__':
+    app.run(debug=True)
 
-# ERROR HANDLING
-@app.errorhandler(404)
-def pageNotFound(error):
-    return "it didnt work"
-
-@app.errorhandler(500)
-def pageNotFound(error):
-    return "internal server error"
-
-if __name__ == "__main__":
-    app.run()
+app = Flask(__name__)
--- example1.py
+++ example1.py
@@ -17,14 +17,21 @@ location = 'h'
 event_types_key = [0,1,0]
 
 # Function Calls
-active_event_types = engine.get_event_types(event_types_key)
-print "Looking for ", playername, active_event_types, "from", season, "/", month
-game_ids = engine.get_game_ids(team,season,month)
-active_event_types = engine.get_event_types(event_types_key)
-ext_ids = engine.filter_game_ids(game_ids, active_event_types,fullyear,location)
-# print ext_ids
-final_urls = engine.parse_for_player(playername, ext_ids)
-print "Found", len(final_urls), active_event_types, ":"
-print json.loads(final_urls)
+def final():
+	active_event_types = engine.get_event_types(event_types_key)
+	print "Looking for ", playername, active_event_types, "from", season, "/", month
+	game_ids = engine.get_game_ids(team,season,month)
+	active_event_types = engine.get_event_types(event_types_key)
+	ext_ids = engine.filter_game_ids(game_ids, active_event_types,fullyear,location)
+	# print ext_ids
+	final_urls = engine.parse_for_player(playername, ext_ids)
+	print "Found", len(final_urls), active_event_types, ":"
+
+	final_dict = {}
+	final_dict['links'] = final_urls
+	final_json = json.dumps(final_dict)
+
+	return final_json
+	# print json.loads(final_dict)
 print "Took", time.time() - start_time, "to run."
 # Usually takes around 35 seconds
